484b6538276362c7580014ad0b1566e4c6003375
daniel <dani.navarro281@gmail.com>
2026-08-16 14:41:00 -0600

    Update versioning; increment versionCode to 2 and versionName to 0.0.2 in build configuration. Enhance release workflow to support AppImage packaging and streamline artifact management for Linux and Windows.

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.dnavarro.poskmp"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "0.0.1"
+        versionCode = 2
+        versionName = "0.0.2"
     }
     packaging {
         resources {
